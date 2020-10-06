b18653871624a37725b002d6e07f06f332b559eb
Sanjit Kumar <sanjitk2018@gmail.com>
2020-10-06 18:17:13 +0530

    readme update

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <Route exact path="/login">
           <SignIn/>
         </Route>
-        <Route path="/home">
+        <Route path="/employee-home">
           <Home/>
         </Route>
       </Switch>
